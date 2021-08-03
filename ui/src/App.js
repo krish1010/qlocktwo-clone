2bd4b4553b9969a49a5fff3c165ee3945e0e8387
krish1010 <krish.krishanu10@gmail.com>
2021-08-04 00:16:09 +0530

    added minutes feature

--- ui/src/App.js
+++ ui/src/App.js
@@ -5,12 +5,14 @@ import { useEffect, useState } from "react";
 function App() {
   const [spell, setSpell] = useState(0);
   const [table, setTable] = useState(false);
+  const [lights, setLights] = useState([0, 0, 0, 0]);
 
   const spellTime = async () => {
     const response = await fetch("http://localhost:5000/spell");
     const data = await response.json();
-    setSpell(data.time);
+    setSpell(data.spell);
     setTable(data.data.grid);
+    setLights(data.lights);
   };
 
   useEffect(() => {
@@ -29,6 +31,22 @@ function App() {
     <div className="center">
       <h3>May the Force be with You.</h3>
       {table && <Grid allElements={table} spell={spell} />}
+      <div>
+        <h2>
+          <span id="1" className={lights[0] ? "on" : "off"}>
+            I
+          </span>
+          <span id="2" className={lights[1] ? "on" : "off"}>
+            I
+          </span>
+          <span id="3" className={lights[2] ? "on" : "off"}>
+            I
+          </span>
+          <span id="4" className={lights[3] ? "on" : "off"}>
+            I
+          </span>
+        </h2>
+      </div>
       <Button onClick={onClick} />
     </div>
   );
