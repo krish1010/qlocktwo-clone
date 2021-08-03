86cc6c38ff12ce93c0cb6b08e2c71d066c2bc062
krish1010 <krish.krishanu10@gmail.com>
2021-08-03 21:04:17 +0530

    removed redundant logs

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,17 +1,15 @@
-import './App.css';
-import Grid from './components/Grid/Grid';
-import { useEffect, useState } from 'react';
+import "./App.css";
+import Grid from "./components/Grid/Grid";
+import { useEffect, useState } from "react";
 function App() {
-  const [spell, setSpell] = useState(0)
-  const [table, setTable] = useState()
+  const [spell, setSpell] = useState(0);
+  const [table, setTable] = useState();
 
   const spellTime = async () => {
     const response = await fetch("http://localhost:5000/spell");
     const data = await response.json();
-    setSpell(data.time)
-    console.log(data.time)
+    setSpell(data.time);
     return data.data.grid;
-
   };
 
   useEffect(() => {
@@ -23,7 +21,7 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
+    <div className="center">
       <h3>May the Force be with You.</h3>
       {table && <Grid allElements={table} spell={spell} />}
     </div>
