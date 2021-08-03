7b441ba5d0c649b7ad94779696b45fae167fd1e9
krish1010 <krish.krishanu10@gmail.com>
2021-08-03 22:28:18 +0530

    fixed formatting

--- ui/src/components/Grid/Grid.js
+++ ui/src/components/Grid/Grid.js
@@ -1,36 +1,45 @@
-import './Grid.css'
-import Data from '../Data/Data'
-import { useState, useEffect } from 'react'
+import "./Grid.css";
+import Data from "../Data/Data";
+import { useState, useEffect } from "react";
 
 function Grid({ allElements, spell }) {
-    const currentTime = spell
-    const [element, setElement] = useState()
+  const currentTime = spell;
+  const [element, setElement] = useState();
 
-    useEffect(() => {
-        setElement(renderTable)
-    }, [])
+  useEffect(() => {
+    setElement(renderTable);
+  }, []);
 
-    const renderTable = () => {
-        return <>{allElements.map((row, index) => (
-            <tbody key={index}>
-                <tr key={index}>
-                    {row.map((ele, idx) =>
-                        <Data key={idx}
-                            data={Object.keys(ele)[0]}
-                            className={currentTime.includes(ele[Object.keys(ele)[0]]) ? 'light' : 'dark'}
-                            id={ele[Object.keys(ele)[0]]} />)
-                    }</tr>
-            </tbody>
-        ))}</>
-    }
-
+  const renderTable = () => {
     return (
-        <>
-            <table className='center'>
-                {element}
-            </table>
-        </>
-    )
+      <>
+        {allElements.map((row, index) => (
+          <tbody key={index}>
+            <tr key={index}>
+              {row.map((ele, idx) => (
+                <Data
+                  key={idx}
+                  data={Object.keys(ele)[0]}
+                  className={
+                    currentTime.includes(ele[Object.keys(ele)[0]])
+                      ? "light"
+                      : "dark"
+                  }
+                  id={ele[Object.keys(ele)[0]]}
+                />
+              ))}
+            </tr>
+          </tbody>
+        ))}
+      </>
+    );
+  };
+
+  return (
+    <>
+      <table className="center">{element}</table>
+    </>
+  );
 }
 
-export default Grid
+export default Grid;
