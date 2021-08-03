2177b67720fe9edce92db62d8a60b7d563199a0c
krish1010 <krish.krishanu10@gmail.com>
2021-08-04 00:32:10 +0530

    removed redundant div

--- ui/src/components/Minutes/Minutes.js
+++ ui/src/components/Minutes/Minutes.js
@@ -2,14 +2,12 @@ import "./Minutes.css";
 
 function Minutes({ lights }) {
   return (
-    <div>
-      <h2>
-        <span className={lights[0] ? "on" : "off"}>I</span>
-        <span className={lights[1] ? "on" : "off"}>I</span>
-        <span className={lights[2] ? "on" : "off"}>I</span>
-        <span className={lights[3] ? "on" : "off"}>I</span>
-      </h2>
-    </div>
+    <h2>
+      <span className={lights[0] ? "on" : "off"}>I</span>
+      <span className={lights[1] ? "on" : "off"}>I</span>
+      <span className={lights[2] ? "on" : "off"}>I</span>
+      <span className={lights[3] ? "on" : "off"}>I</span>
+    </h2>
   );
 }
 
