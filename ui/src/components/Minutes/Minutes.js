b757efa638f342a9e334e0b9efc65af9027c1a02
krish1010 <krish.krishanu10@gmail.com>
2021-08-04 04:42:59 +0530

    refactored span

--- ui/src/components/Minutes/Minutes.js
+++ ui/src/components/Minutes/Minutes.js
@@ -3,10 +3,11 @@ import "./Minutes.css";
 function Minutes({ lights }) {
   return (
     <h2>
-      <span className={lights[0] ? "on" : "off"}>I</span>
-      <span className={lights[1] ? "on" : "off"}>I</span>
-      <span className={lights[2] ? "on" : "off"}>I</span>
-      <span className={lights[3] ? "on" : "off"}>I</span>
+      {lights.map((light, idx) => (
+        <span key={idx} className={light ? "on" : "off"}>
+          I
+        </span>
+      ))}
     </h2>
   );
 }
